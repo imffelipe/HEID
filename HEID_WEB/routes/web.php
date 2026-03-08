3b0c40d6a9d8f604854df2f0c169bfc52bdb3ce8
imffelipe <rs.mafla44@gmail.com>
2026-03-07 19:11:54 -0500

    Cambio de rutas

--- HEID_WEB/routes/web.php
+++ HEID_WEB/routes/web.php
@@ -6,17 +6,14 @@ Route::get('/', function () {
     return view('index');
 })->name('home');
 
-// Página: Cómo funciona
 Route::get('/como-funciona', function () {
     return view('pages.como-funciona');
 })->name('como-funciona');
 
-// Página: Sobre HEID
 Route::get('/nosotros', function () {
     return view('pages.nosotros');
 })->name('nosotros');
 
-// Módulos
 Route::prefix('modulos')->name('modulos.')->group(function () {
     Route::get('/kids',     fn() => view('modulos.kids'))    ->name('kids');
     Route::get('/silver',   fn() => view('modulos.silver'))  ->name('silver');
